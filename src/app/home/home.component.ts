78626ca7f7507d262d7db9712294eb4245d20ae8
ffacon <francois.facon@gmail.com>
2017-02-13 18:02:02 +0100

    Update Home.component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,64 @@
 import { Component, OnInit } from '@angular/core';
 
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/operator/toArray';
+
+import {NewsService} from '../services/news.Service';
+import {News} from '../beans/news';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+   providers: [NewsService]
+
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  message: string = 'Welcome in our shop!!!';
+	news: News[];
+	newsOfTheDay: News= {};
+	nextNews: News = {};
 
-  ngOnInit() {
-  }
+
+	constructor(private newsService: NewsService) {}
+
+	ngOnInit(){
+		this.updateNews();
+	}
+
+	updateNews= () => {
+		this.newsService
+			.getNews()
+			.toArray()
+			.subscribe((news: News[]) => {
+				this.news = news;
+			});
+
+
+		this.newsService.randomNews()
+		.subscribe((news: News) => {
+			this.newsOfTheDay = news;
+		});
+	}
+
+	addLike= (news: News) => {
+		this.newsService.addLike(news);
+	}
+
+	deleteNews= (news: News) => {
+		this.newsService.deleteNews(news)
+		.subscribe( (response: any) => {
+			this.updateNews();
+		} );
+	}
+
+	addNews= () => {
+		this.newsService.addNews(this.nextNews)
+		.subscribe((response: any) => {
+			this.updateNews();
+		});		
+	}
+
 
 }
