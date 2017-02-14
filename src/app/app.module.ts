ffbb5d5f3f214f3d3752203b58b00dd39ae95068
ffacon <francois.facon@gmail.com>
2017-02-14 18:04:10 +0100

    Update AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,11 @@ import { FilterFieldPipe } from './pipes/filter-field.pipe';
 import { OrderByPipe } from './pipes/order-by.pipe';
 import { UpdateDataPipe } from './pipes/update-data.pipe';
 
+import {UserService} from './services/user.service';
+import {LocalStorageService} from './services/LocalStorage.service';
+import {NewsService} from './services/news.service';
+
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -38,7 +43,7 @@ import { UpdateDataPipe } from './pipes/update-data.pipe';
     HttpModule,
     routing
   ],
-  providers: [],
+  providers: [UserService,  LocalStorageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
