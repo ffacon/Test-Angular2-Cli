9d8c84834c0e2a41ca9561cde28ada4f45442b46
ffacon <francois.facon@gmail.com>
2017-02-15 18:05:38 +0100

    Update App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 
+
+import {BooksService} from './services/books.service';
+import {DataContainerService} from './services/dataContainer.service';
+import {ContactService} from './services/contact.service';
+import {UserService} from './services/user.service';
+
+import { Router }   from '@angular/router';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [UserService ]
 })
 export class AppComponent {
-  title = 'app works!';
+  constructor(private router: Router, public userService: UserService) {}
+
+	logout= (): boolean => {
+		this.userService.logout();
+		this.router.navigate(['Home']);
+		return false;
+	}
 }
