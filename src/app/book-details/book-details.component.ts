36499e14563c03b3698c5128a8f2d76c7b35b04a
ffacon <francois.facon@gmail.com>
2017-02-25 16:55:04 +0100

    Update book details

--- src/app/book-details/book-details.component.ts
+++ src/app/book-details/book-details.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+import {UserService} from '../services/user.service';
+import {BooksService} from '../services/books.service';
+import {Book} from '../beans/book';
+import { ActivatedRoute  }  from '@angular/router';
+
 @Component({
   selector: 'app-book-details',
   templateUrl: './book-details.component.html',
@@ -7,9 +12,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookDetailsComponent implements OnInit {
 
-  constructor() { }
+  public book: Book;
+  private sub: any;
+  private bookId :number;
 
-  ngOnInit() {
-  }
+	constructor(
+		private route: ActivatedRoute,
+		public bookService: BooksService,
+		public userService: UserService){}
+
+	ngOnInit(): void {
+		let that = this;
+		this.sub = this.route.params.subscribe( params => {
+       this.bookId = +params['id']; // (+) converts string 'bookId' to a number
+
+       // In a real app: dispatch action to load the details here.
+    });
+		
+
+
+		this.bookService.getBook(this.bookId)
+			.then((book: Book) => {
+				that.book = book;
+			});
+	}
+
+
+	getImagePath = (): string => {
+		return '';
+	}
+
+
+	getStarsImagePath= (): string => {
+		return '';
+	}
+
 
 }
