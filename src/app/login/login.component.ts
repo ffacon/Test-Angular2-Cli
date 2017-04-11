6fdf38a86b1aae5df81092966485a1856b12f97f
ffacon <francois.facon@gmail.com>
2017-04-11 17:26:41 +0200

    PW8 : login, basket & redirection

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {Http, Response} from '@angular/http';
+import { Router } from '@angular/router';
+
+import { User } from '../beans/user';
+import {UserService} from '../services/user.service';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +12,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  errorMessage: string= undefined;
+
+	//Form fields
+	login: string;
+	password: string;
+
+	constructor(private router: Router, private userService: UserService) {}
+
+	logUser= () => {
+
+		this.userService.login(this.login, this.password).toPromise()
+			.then((user: User) => {
+				this.router.navigate(['home']);
+			})
+			.catch((err: Response) => {
+				console.error(err);
+				this.errorMessage = `Server answer status: ${err.status}`;
+				if (err.status === 401){
+					this.errorMessage += ' (unauthorized)';
+				}
+			});
+
+	}
 
   ngOnInit() {
   }
