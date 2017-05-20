7175962af410f2525e525c83984b80ec2c563ab0
ffacon <francois.facon@gmail.com>
2017-05-20 17:39:05 +0200

    update e2e data

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
-import { Ng2EComPage } from './app.po';
+import { AppPage } from './app.po';
 
-describe('ng2-e-com App', function() {
-  let page: Ng2EComPage;
+describe('ng2-e-com App', () => {
+  let page: AppPage;
 
   beforeEach(() => {
-    page = new Ng2EComPage();
+    page = new AppPage();
   });
 
-  it('should display message saying app works', () => {
+  it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
   });
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,6 +1,6 @@
-import { browser, element, by } from 'protractor';
+import { browser, by, element } from 'protractor';
 
-export class Ng2EComPage {
+export class AppPage {
   navigateTo() {
     return browser.get('/');
   }
