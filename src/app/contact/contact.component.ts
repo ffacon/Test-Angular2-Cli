68f548bec4e3cc500cab1c6a1572d0a177a70027
ffacon <francois.facon@gmail.com>
2017-06-16 11:53:37 +0200

    Update contact component

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
+  public email: string= "ecom@worldline.com";
+  phone: string= "+33 xxxxxxxx" 
+  address: string= "rue de la pointe Seclin"
+  message="";
+
   constructor() { }
+  
+  updateTextContent  = (data:any) =>{
+    this.message = data.target.value;
+  }
+
+  sendMessage = () => {
+    this.message ="";
+  }
 
   ngOnInit() {
+
   }
 
 }
