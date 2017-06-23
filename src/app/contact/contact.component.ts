771a2bb1e3b5df03e6d67f5a3183a69afdd1d86d
ffacon <francois.facon@gmail.com>
2017-06-23 15:50:42 +0200

    PW6 declare formGroup

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,5 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import {ContactService} from '../services/contact.service';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+
+function containsValidCharacters(c:FormControl){
+  let specialChars= ['\\','<', '>', '&' ];
+  for (let i in specialChars){
+    if ( c.value !== undefined && c.value.indexOf(specialChars[i]) != -1 ){
+      return {'containsValidCharacters': true };
+    }
+   }
+  return null;
+ }
 
 @Component({
   selector: 'app-contact',
@@ -12,12 +23,17 @@ export class ContactComponent implements OnInit {
   phone: string; 
   address: string;
   message="";
+  controlGroup: FormGroup;
+  messageCtrl: FormControl;
 
-  constructor(contactService: ContactService) { 
+
+  constructor(contactService: ContactService, fb:FormBuilder) { 
     this.email = contactService.email ;
     this.phone = contactService.phone ;
     this. address = contactService.address;
     
+    this.messageCtrl = fb.control('',[Validators.required , containsValidCharacters ]);
+    this.controlGroup = fb.group({'messageCtrl':this.messageCtrl});
   }
   
   updateTextContent  = (data:any) =>{
