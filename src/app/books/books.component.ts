aa1c9314a3334a518989c9cbcaec7256168b155c
ffacon <francois.facon@gmail.com>
2017-07-08 19:52:27 +0200

    PW7 update books components

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -1,7 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Book} from '../beans/book';
 import { BooksService} from '../services/books.service';
+import { DataContainerService} from '../services/data-container.service'
+import { FilterFieldPipe } from '../pipes/filter-field.pipe';
+import { UpdateDataPipe } from '../pipes/update-data.pipe';
+import { OrderByPipe } from '../pipes/order-by.pipe';
 
+ 
 @Component({
   selector: 'app-books',
   templateUrl: './books.component.html',
@@ -14,7 +19,7 @@ export class BooksComponent implements OnInit {
   bookNameFilter: string = '';
   books: Book[];
 
-  constructor(private booksService:BooksService ) { }
+  constructor(private booksService: BooksService, public dataContainer: DataContainerService ) { }
 
   ngOnInit() {
     this.booksService.getBooks().then((books: Book[]) => {
