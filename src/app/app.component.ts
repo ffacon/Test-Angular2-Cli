3bbd76c8673c9ee74630ae37aa7aad9771c6e699
ffacon <francois.facon@gmail.com>
2017-07-19 12:40:50 +0200

    PW8 add missing basket ref

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { Component } from '@angular/core';
 import {BooksService} from './services/books.service';
 import {ContactService} from './services/contact.service';
 import {UserService} from './services/user.service';
+import { Basket } from './beans/basket'
 
 import { Router }   from '@angular/router';
 
