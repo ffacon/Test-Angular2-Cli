8ea530da88f4b83912e9602b848329f1d3344073
ffacon <francois.facon@gmail.com>
2017-08-01 17:08:46 +0200

    Fix book-details component spec

--- src/app/basket/basket.component.spec.ts
+++ src/app/basket/basket.component.spec.ts
@@ -2,18 +2,46 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 import { BasketComponent } from './basket.component';
+
+import {Http, Response, ResponseOptions, BaseRequestOptions, ConnectionBackend, HttpModule} from '@angular/http';
+import {FormsModule, FormBuilder, FormGroup, FormControl, Validators, ReactiveFormsModule} from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { MockBackend } from '@angular/http/testing';
+import { MockRouter, MockActivatedRoute } from '../test/mocks/route';
+import { MockUserService } from '../test/mocks/user.service';
+
+import { UserService } from '../services/user.service';
+import { LocalStorageService } from '../services/localStorage.service';
 
 describe('BasketComponent', () => {
   let component: BasketComponent;
   let fixture: ComponentFixture<BasketComponent>;
 
+  let mockUserService: MockUserService; 
+
   beforeEach(async(() => {
+    mockUserService = new MockUserService();
+
     TestBed.configureTestingModule({
-      declarations: [ BasketComponent ]
-    })
-    .compileComponents();
+      declarations: [ BasketComponent ],
+       imports: [ FormsModule, ReactiveFormsModule, RouterTestingModule ],
+      providers: [
+        LocalStorageService,
+        {provide: UserService, useClass: mockUserService},
+        {provide: Router, useValue: MockRouter},
+        {
+          provide: Http, useFactory: (backend: ConnectionBackend, defaultOptions: BaseRequestOptions) => {
+          return new Http(backend, defaultOptions);
+        }, deps: [MockBackend, BaseRequestOptions]
+        },
+        {provide: MockBackend, useClass: MockBackend},
+        {provide: BaseRequestOptions, useClass: BaseRequestOptions}
+      ]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -22,7 +50,5 @@ describe('BasketComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  
 });
