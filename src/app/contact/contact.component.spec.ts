af5244d2d0e9634779359a5700068568401fc7a6
ffacon <francois.facon@gmail.com>
2017-08-03 17:09:50 +0200

    Fix contact component spec

--- src/app/contact/contact.component.spec.ts
+++ src/app/contact/contact.component.spec.ts
@@ -3,7 +3,11 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
+import {Http, Response, ResponseOptions} from '@angular/http';
+import {FormsModule, FormBuilder, FormGroup, FormControl, Validators, ReactiveFormsModule} from '@angular/forms';
+
 import { ContactComponent } from './contact.component';
+import {ContactService} from '../services/contact.service';
 
 describe('ContactComponent', () => {
   let component: ContactComponent;
@@ -11,7 +15,9 @@ describe('ContactComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ContactComponent ]
+      declarations: [ ContactComponent ],
+      imports: [ FormsModule, ReactiveFormsModule ],
+      providers: [ContactService]
     })
     .compileComponents();
   }));
