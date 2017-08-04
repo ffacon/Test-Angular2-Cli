393bd3b78d874b88a71e823b3c0dcf7e45bc871a
ffacon <francois.facon@gmail.com>
2017-08-04 17:10:24 +0200

    Fix home component spec

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -4,14 +4,34 @@ import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
 import { HomeComponent } from './home.component';
+import { UserService } from '../services/user.service';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import { MockUserService } from '../test/mocks/user.service';
+import { Http, ConnectionBackend, BaseRequestOptions } from '@angular/http';
+import { MockBackend } from '@angular/http/testing';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
 
+  let mockUserService: MockUserService;
+  
+  mockUserService = new MockUserService();
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
+      declarations: [ HomeComponent ],
+      imports: [ FormsModule, ReactiveFormsModule , RouterTestingModule],
+      providers: [
+        {
+          provide: Http, useFactory: (backend: ConnectionBackend, defaultOptions: BaseRequestOptions) => {
+          return new Http(backend, defaultOptions);
+        }, deps: [MockBackend, BaseRequestOptions]
+        }, 
+        {provide: MockBackend, useClass: MockBackend},
+        {provide: BaseRequestOptions, useClass: BaseRequestOptions},
+        {provide: UserService, useValue: mockUserService}]
     })
     .compileComponents();
   }));
