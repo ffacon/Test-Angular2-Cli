4535ed359237a7670cdc72494bec906050884f8d
ffacon <francois.facon@gmail.com>
2017-08-05 17:11:07 +0200

    Fix update pipe spec

--- src/app/pipes/update-data.pipe.spec.ts
+++ src/app/pipes/update-data.pipe.spec.ts
@@ -1,11 +1,29 @@
 /* tslint:disable:no-unused-variable */
 
+import {ReflectiveInjector, Injector} from '@angular/core';
 import { TestBed, async } from '@angular/core/testing';
 import { UpdateDataPipe } from './update-data.pipe';
+import { DataContainerService } from '../services/data-container.service';
+
+
+
+let parentInjector: Injector;
+let dataContainerMock: any;
+
+beforeEach(() => {
+
+  dataContainerMock = { filteredBooks: ''};
+  TestBed.configureTestingModule({
+    providers: [
+      {provide : DataContainerService, 
+       useClass: dataContainerMock}]
+  }).compileComponents();
+
+});
 
 describe('UpdateDataPipe', () => {
   it('create an instance', () => {
-    const pipe = new UpdateDataPipe();
+    const pipe = new UpdateDataPipe(dataContainerMock);
     expect(pipe).toBeTruthy();
   });
 });
