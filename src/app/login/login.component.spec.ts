ccd93e893202d5ec70c85a040b6de05dec17dd68
ffacon <francois.facon@gmail.com>
2017-08-05 17:10:41 +0200

    Fix login component spec

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -4,14 +4,25 @@ import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
 import { LoginComponent } from './login.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import { UserService } from '../services/user.service';
+import { MockUserService } from '../test/mocks/user.service';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
 
+  let mockUserService: MockUserService;
+
+  mockUserService = new MockUserService();
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      imports: [ FormsModule, ReactiveFormsModule , RouterTestingModule],
+      declarations: [ LoginComponent ],
+      providers: [
+        {provide: UserService, useValue: mockUserService}]
     })
     .compileComponents();
   }));
