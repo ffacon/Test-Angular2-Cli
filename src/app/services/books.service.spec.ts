d8abf195021f8c90efe5280627db1c7dd07ec679
ffacon <francois.facon@gmail.com>
2017-08-06 17:11:30 +0200

    Fix books services spec

--- src/app/services/books.service.spec.ts
+++ src/app/services/books.service.spec.ts
@@ -2,11 +2,22 @@
 
 import { TestBed, async, inject } from '@angular/core/testing';
 import { BooksService } from './books.service';
+import { BaseRequestOptions, Http, ConnectionBackend, Response, ResponseOptions } from '@angular/http';
+import { MockBackend } from '@angular/http/testing';
 
 describe('BooksService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [BooksService]
+       providers: [
+        {
+          provide: Http, useFactory: (backend: ConnectionBackend, defaultOptions: BaseRequestOptions) => {
+          return new Http(backend, defaultOptions);
+        }, deps: [MockBackend, BaseRequestOptions]
+        },
+        {provide: BooksService, useClass: BooksService},
+        {provide: MockBackend, useClass: MockBackend},
+        {provide: BaseRequestOptions, useClass: BaseRequestOptions}
+      ]
     });
   });
 
