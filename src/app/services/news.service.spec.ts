674ccfddf8e5847625eb8d05440497606db8ed48
ffacon <francois.facon@gmail.com>
2017-08-08 17:12:20 +0200

    Fix news services spec

--- src/app/services/news.service.spec.ts
+++ src/app/services/news.service.spec.ts
@@ -2,11 +2,25 @@
 
 import { TestBed, async, inject } from '@angular/core/testing';
 import { NewsService } from './news.service';
+import { BaseRequestOptions, Http, ConnectionBackend, Response, ResponseOptions } from '@angular/http';
+import { MockBackend } from '@angular/http/testing';
+import { LocalStorageService } from './localStorage.service'
 
 describe('NewsServiceService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [NewsService]
+      providers: [
+        {
+          provide: Http, useFactory: (backend: ConnectionBackend, defaultOptions: BaseRequestOptions) => {
+          return new Http(backend, defaultOptions);
+        }, deps: [MockBackend, BaseRequestOptions]
+        },
+        {provide: MockBackend, useClass: MockBackend},
+        {provide: BaseRequestOptions, useClass: BaseRequestOptions},
+        {provide: NewsService, useClass: NewsService},
+        {provide: LocalStorageService, useClass: LocalStorageService}
+        
+      ]
     });
   });
 
