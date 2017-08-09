a33087b58a3a81a57e362c6426ca68c3d8054292
ffacon <francois.facon@gmail.com>
2017-08-09 17:12:42 +0200

    Fix localStorage services spec

--- src/app/services/localStorage.service.spec.ts
+++ src/app/services/localStorage.service.spec.ts
@@ -2,11 +2,23 @@
 
 import { TestBed, async, inject } from '@angular/core/testing';
 import { LocalStorageService } from './localStorage.service';
+import { BaseRequestOptions, Http, ConnectionBackend, Response, ResponseOptions } from '@angular/http';
+import { MockBackend } from '@angular/http/testing';
+
 
 describe('LocalStorageService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [LocalStorageService]
+      providers: [
+        {
+          provide: Http, useFactory: (backend: ConnectionBackend, defaultOptions: BaseRequestOptions) => {
+          return new Http(backend, defaultOptions);
+        }, deps: [MockBackend, BaseRequestOptions]
+        },
+        {provide: LocalStorageService, useClass: LocalStorageService},
+        {provide: MockBackend, useClass: MockBackend},
+        {provide: BaseRequestOptions, useClass: BaseRequestOptions}
+      ]
     });
   });
 
