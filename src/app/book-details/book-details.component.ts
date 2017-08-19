7b2bf0b8cec3f7590b9e4f9edc7bb4a5e32a68fe
ffacon <francois.facon@gmail.com>
2017-08-19 19:01:07 +0200

    PW11 update Book-details components

--- src/app/book-details/book-details.component.ts
+++ src/app/book-details/book-details.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import {UserService} from '../services/user.service';
 import {BooksService} from '../services/books.service';
 import {Book} from '../beans/book';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute  }  from '@angular/router';
 
 @Component({
   selector: 'app-book-details',
@@ -17,43 +17,35 @@ export class BookDetailsComponent implements OnInit {
   private bookId :number;
 
 	constructor(
-		private router: Router,
 		private route: ActivatedRoute,
 		public bookService: BooksService,
 		public userService: UserService){}
 
 	ngOnInit(): void {
-		let that = this;
 		this.sub = this.route.params.subscribe( params => {
         this.bookId = +params['id']; // (+) converts string 'bookId' to a number
-
-       // In a real app: dispatch action to load the details here.
-    });
-		
-
-
-		this.bookService.getBook(this.bookId)
-			.then((book: Book) => {
-				that.book = book;
+       	// In a real app: dispatch action to load the details here.
+	   	this.bookService.getBook(this.bookId)
+			.subscribe((book: Book) => {
+				this.book = book;
 			});
+    	});
 	}
 
 
 	getImagePath = (): string => {
-		if(!this.book){
+		if (!this.book){
 			return '';
 		}
-		return '/data/imgs/books/'+ this.book.id + '.jpg'; 
-		
+		return '/data/imgs/books/' + this.book.id + '.jpg';
 	}
 
-
 	getRatingAverage= (): string => {
-		if(!this.book){
+		if (!this.book){
 			return '';
 		}
 		return this.bookService.convertFromRating(this.bookService.getRatingAverage(this.book));
-	} 
+	}
 
 	getStarsImagePath= (): string => {
 		if (!this.book){
@@ -62,9 +54,6 @@ export class BookDetailsComponent implements OnInit {
 		return 'assets/styles/ktheme/img/' + this.getRatingAverage() + '-stars.svg'
 	}
 
-	addToBasket(book: Book) {
-		this.userService.basket.addProduct(book);
-		this.router.navigate(['basket']);
-  }
+	
 
 }
