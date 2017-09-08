8459dbc6995f67ad70ed7b32421ca0081a8bd87a
ffacon <francois.facon@gmail.com>
2017-09-08 19:11:32 +0200

    Clean up books.components

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Book} from '../beans/book';
-import { BooksService} from '../services/books.service';
+
 
 @Component({
   selector: 'app-books',
@@ -9,15 +8,8 @@ import { BooksService} from '../services/books.service';
 })
 export class BooksComponent implements OnInit {
 
-  books: Book[];
+  constructor( ) { }
 
-  constructor(private booksService:BooksService ) { }
-
-  ngOnInit() {
-    this.booksService.getBooks()
-    .then((books: Book[]) => {
-      this.books = books;
-    })
-  }
+  ngOnInit() { }
 
 }
