eb09f3bd93dd1c01637a177e751374bc46516867
ffacon <francois.facon@gmail.com>
2017-10-08 22:14:20 +0200

    PW3: Add route for contact

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
+  public email: string= "ecom@worldline.com";
+  phone: string= "+33 xxxxxxxx" 
+  address: string= "rue de la pointe Seclin"
+  message="";
+
   constructor() { }
+  
+  updateTextContent  = (data:any) =>{
+    this.message = data.target.value;
+    console.log(this.message);
+  }
+
+  sendMessage = () => {
+    this.message ="";
+  }
 
   ngOnInit() {
+
   }
 
 }
